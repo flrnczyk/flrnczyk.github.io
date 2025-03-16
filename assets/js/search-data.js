56ab870ab4561129751cbbde7f2d585cd1b6f843
flrnczyk <132750368+flrnczyk@users.noreply.github.com>
2025-03-16 05:34:21 +0000

    Deploying to gh-pages from @ flrnczyk/flrnczyk.github.io@c6c9807dd86bd5b21398617276bee8177aab1c16 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "",
+          description: "📄",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
