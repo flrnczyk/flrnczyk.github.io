cafd88c636fbbaab820951248947c4cf87b3831c
flrnczyk <132750368+flrnczyk@users.noreply.github.com>
2025-03-16 05:43:15 +0000

    Deploying to gh-pages from @ flrnczyk/flrnczyk.github.io@f3cfb0bd80d087668e7362b32dd54134d8fea4a7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,18 +23,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-        
-      },
-    },{id: "post-모래는-뭐래-독후감",
+        },{id: "post-모래는-뭐래-독후감",
       
         title: "『모래는 뭐래』 독후감",
       
@@ -45,32 +34,22 @@ ninja.data = [{
           window.location.href = "/blog/2023/book/";
         
       },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-        
-      },
     },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-collaborated-with-graphic-designer-makoto-zhang-on-his-zine-reverse-waterfall",
+          title: 'I collaborated with graphic designer Makoto Zhang on his zine Reverse Waterfall.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-i-graduated",
+          title: 'I graduated!',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-linmin-zhang-s-and-my-manuscript-comparative-morphemes-are-additive-particles-english-er-more-vs-chinese-gèng-is-now-on-lingbuzz",
+          title: 'Linmin Zhang’s and my manuscript Comparative Morphemes are Additive Particles: English -er/more vs....',
+          description: "",
+          section: "News",},{id: "news-dingfan-lin-and-i-will-be-presenting-a-poster-on-degree-reduplication-at-glow47",
+          title: 'Dingfan Lin and I will be presenting a poster on Degree Reduplication at...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
