c8712aa87e0151f585fa47d702c688db1b53caa3
flrnczyk <132750368+flrnczyk@users.noreply.github.com>
2025-03-18 11:23:08 -0400

    Create bd-analytics.js

---
+++ assets/js/bd-analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e23431111bc3a7e23058b9de56847d5c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
