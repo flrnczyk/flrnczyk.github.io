6c9904b5ba020de0b2438b7690ba09de0e2c69ad
flrncykz <132750368+flrnczyk@users.noreply.github.com>
2025-03-18 11:32:54 -0400

    deletion

--- assets/js/bd-analytics.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?e23431111bc3a7e23058b9de56847d5c";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
